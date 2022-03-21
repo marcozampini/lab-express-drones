10bb81590d12f08c79dde87f52ffd0d31ca5d9af
Marco Zampini <marco@bloom-it.io>
2022-03-22 00:01:34 +0100

    Iteration 2 | List the drones

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-// require the Drone model here
+const droneModel = require('../models/Drone.model')
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
-});
+router.get('/drones', async (req, res, next) => {
+  const drones = await droneModel.find()
+  res.render('drones/list', { drones })
+})
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+})
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
-});
+})
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+})
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-});
+})
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
-});
+})
 
-module.exports = router;
+module.exports = router
